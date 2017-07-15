12db9b341b9992a2b029a90e59c5a27c3639b2b7
Taylor Wong <taylorwong@Taylors-MacBook-Pro.local>
2017-07-15 04:30:45 -0400

    blog py files

---
+++ postConfirmed.py
@@ -0,0 +1,106 @@
+#! /usr/bin/python
+import cgitb
+cgitb.enable()
+
+print 'content-type: text/html\n'
+
+import cgi
+fromQS = cgi.FieldStorage()
+
+title = fromQS.getvalue('title')
+postText = fromQS.getvalue('postText')
+
+import csvToDictModule
+accounts = csvToDictModule.csvToDict('posts.csv')
+#print logIn
+
+def confirmPost():
+    dest = open('posts.csv', 'a', 0)
+    dest.write('\n' + title + ',' + postText)
+    print '''
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="description" content="">
+                <meta name="author" content="">
+
+                <title>How Are Our Pen Pals Doing?</title>
+
+                <!-- Bootstrap Core CSS -->
+                <link href="css/bootstrap.min.css" rel="stylesheet">
+
+                <!-- Custom CSS -->
+                <link href="css/blog-home.css" rel="stylesheet">
+
+                <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                <!--[if lt IE 9]>
+                    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+                    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+                <![endif]-->
+
+            </head>
+
+            <body>
+
+                <!-- Navigation***************HIIIIIIII JOYCEEEEEEEEEEEEEE THIS IS THE NAVIGATION TAB THINGGGGGGGGGGGGGGGGG WOOOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+             -->
+                <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Start Bootstrap</a>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="#">About</a>
+                                </li>
+                                <li>
+                                    <a href="#">Services</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.container -->
+                </nav>
+
+                <!-- Page Content -->
+                <div class="container">
+
+                    <div class="row">
+
+                        <!-- Blog Entries Column -->
+                        <div class="col-md-8">
+
+                            <h1 class="page-header">
+                                How Are Our Pen Pals Doing?
+                                <small>A Lifestyle Blog from Our Members</small>
+                            </h1>
+
+                            <!-- First Blog Post -->
+                            <h2>
+                                Your submission has been uploaded!
+                            </h2>
+                            
+                            <form action="blogHome.py" method="POST">
+
+                            <input type="submit" value="Return to Blog Home">
+                            </form>
+                            '''
